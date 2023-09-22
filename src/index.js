3183d180e43a1994b8ee5832879bdc27e382a4d1
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-09-22 18:27:54 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  const openBrackets = bracketsConfig.map((config) => config[0]);
+  const closeBrackets = bracketsConfig.map((config) => config[1]);
+  for (let i = 0; i < str.length; i++) {
+    let carrentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+    if (
+      openBrackets.includes(carrentSymbol) &&
+      closeBrackets.includes(carrentSymbol) &&
+      carrentSymbol === topElement
+    ) {
+      stack.pop();
+    } else if (openBrackets.includes(carrentSymbol)) {
+      stack.push(carrentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      } else {
+        if (openBrackets[closeBrackets.indexOf(carrentSymbol)] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return stack.length === 0;
+};
